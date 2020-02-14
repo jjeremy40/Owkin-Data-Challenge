94abf674e863fcb419a32b02b645b99558cb0894
Jaspar <jeremyjaspar2@gmail.com>
2020-02-14 09:08:33 +0100

    no message

--- Meth1.py
+++ Meth1.py
@@ -1,9 +1,101 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Wed Feb 12 17:54:46 2020
 
-@author: jeremyjaspar
+
+"""Methode using a CNN neural network and use as features the 
+sum (normalized) of all the masks for eachs patient. So each patient will 
+be characterized by 1 image with size 92x92x1. We use all the examples from x_train 
+for the training et all the examples from x_test for the testing the algorithme (we don't deparate 
+dataset public and dataset private)
 """
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from metrics import *
+import tensorflow as tf
+import tensorflow.keras.layers as tkl
+import os
+
+#%% Data Prep :
+
+#train_base :
+train_base = np.empty((300, 92, 92))
+basepath = 'x_train/images/'
+i = 0
+for file in sorted(os.listdir(basepath)):
+    scan = np.load(os.path.join(basepath,file))['mask']
+    train_base[i,:,:] = scan.sum(axis=2)/92
+    i += 1
+
+    
+#test_base :
+test_base = np.empty((125, 92, 92))
+basepath = 'x_test/images/'
+i = 0
+for file in sorted(os.listdir(basepath)):
+    scan = np.load(os.path.join(basepath,file))['mask']
+    test_base[i,:,:] = scan.sum(axis=2)/92
+    i += 1
+    
+
+#train_label :
+train_output = pd.read_csv('x_train/output.csv', index_col=0).reset_index().values
+train_label = train_output[:, 1]
+id_patient_train = train_output[:, 0]
+event_train = train_output[:,2]
+
+#test_label : Performances
+test_output_real = pd.read_csv('x_test/output.csv', index_col=0)
+buff = test_output_real.reset_index().values
+id_patient = buff[:, 0]
+event = buff[:,2]
+
+
+#For the CNN:
+train_base=train_base.reshape(len(train_base),train_base.shape[1], train_base.shape[2], 1)
+test_base=test_base.reshape(len(test_base),test_base.shape[1], test_base.shape[2], 1)
+
+
+#%% Model Training :
+
+model = tf.keras.models.Sequential()
+model.add(tkl.Conv2D(10, (20, 20), activation='relu', input_shape=(92, 92, 1)))
+model.add(tkl.MaxPool2D(pool_size=(4, 4)))
+model.add(tkl.Flatten(data_format='channels_last'))
+model.add(tkl.Dense(100, activation='relu'))
+model.add(tkl.Dense(20, activation='relu'))
+model.add(tkl.Dense(1, activation='relu'))
+
+model.summary()
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+
+#%% Learning :
+model.fit(train_base, train_label, batch_size=30, epochs=200)
+
+#%%Performance :
+
+test = model.predict(test_base)
+test_output = pd.DataFrame(data = test[:,0], index = id_patient, columns=['SurvivalTime'], dtype=np.int64)
+test_output.insert(1,'Event',event)
+test_output.index.name = 'PatientID'
+print(cindex(test_output_real, test_output))
+
+#%%Performance :
+
+test = model.predict(train_base)
+test_output = pd.DataFrame(data = test[:,0], index = id_patient_train, columns=['SurvivalTime'], dtype=np.int64)
+test_output.insert(1,'Event',event_train)
+test_output.index.name = 'PatientID'
+print(cindex(train_output, test_output))
+
+#%%
+train_output = pd.read_csv('x_train/output.csv', index_col=0)
+test_output_real = pd.read_csv('x_test/output.csv', index_col=0)
+
+print(cindex(train_output, train_output))
+print(cindex(test_output_real, test_output_real))
+
 
 
---
+++ Meth2.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+"""Method using the radiomics and clinical data using a simple MLP.
+We concatenate them to get a vector 1x57 per patient. We use all the datas from 
+x_train for the training et we separate de datas between dataset public and 
+dataset private to test the algorithme."""
+
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt 
+from metrics import *
+import tensorflow as tf
+import tensorflow.keras.layers as tkl
+import os
+
+#%% Data Prep:
+
+#train_base:
+train_buff_radiomics = pd.read_csv('x_train/features/radiomics.csv',index_col=0)
+train_buff_clinical = pd.read_csv('x_train/features/clinical_data.csv',index_col=0)
+
+train_base_clinical = train_buff_clinical[['Nstage','Tstage','age']].values
+train_buff_radiomics = train_buff_radiomics.values
+train_base_radiomics = train_buff_radiomics[2:,:].astype(np.float64)
+
+#test_base for Dataset l1 public and Dataset l2 private :
+test_buff_radiomics = pd.read_csv('x_test/features/radiomics.csv',index_col=0)
+test_buff_clinical = pd.read_csv('x_test/features/clinical_data.csv',index_col=0)
+
+test_base_clinical_public = test_buff_clinical.loc[test_buff_clinical['SourceDataset']=='l1',:][['Nstage','Tstage','age']].values
+test_base_clinical_private = test_buff_clinical.loc[test_buff_clinical['SourceDataset']=='l2',:][['Nstage','Tstage','age']].values
+
+test_buff_radiomics = test_buff_radiomics.values
+test_buff_radiomics = test_buff_radiomics[2:,:]
+test_base_radiomics_public = test_buff_radiomics[test_buff_clinical['SourceDataset']=='l1',:].astype(np.float64)
+test_base_radiomics_private = test_buff_radiomics[test_buff_clinical['SourceDataset']=='l2',:].astype(np.float64)
+
+
+#train_label :
+train_output = pd.read_csv('x_train/output.csv', index_col=0).reset_index().values
+train_label = train_output[:, 1]
+
+#test_label : Performances
+test_output_real = pd.read_csv('x_test/output.csv', index_col=0)
+test_output_real_public = test_output_real.loc[test_buff_clinical['SourceDataset']=='l1',:]
+test_output_real_private = test_output_real.loc[test_buff_clinical['SourceDataset']=='l2',:]
+buff = test_output_real.reset_index().values
+id_patient_public = buff[test_buff_clinical['SourceDataset']=='l1', 0]
+id_patient_private = buff[test_buff_clinical['SourceDataset']=='l2', 0]
+event_public = buff[test_buff_clinical['SourceDataset']=='l1',2]
+event_private = buff[test_buff_clinical['SourceDataset']=='l2',2]
+
+
+del train_buff_clinical, train_buff_radiomics
+del train_output, buff, test_buff_clinical, test_buff_radiomics, test_output_real
+
+
+#%% Model :
+model = tf.keras.models.Sequential()
+model.add(tkl.Dense(100, input_shape=(53,), activation='relu'))
+model.add(tkl.Dense(100, activation='relu'))
+model.add(tkl.Dense(1, activation='relu')) 
+
+model.summary()
+
+model.compile(loss='mean_squared_error', optimizer='adam', metrics=['accuracy'])
+
+
+#%% Learning :
+model.fit(train_base_radiomics, train_label, batch_size=30, epochs=150)
+
+
+#%%Performance :
+#on public test:
+test = model.predict(test_base_radiomics_public)
+test_output_public = pd.DataFrame(data = test[:,0], index = id_patient_public, columns=['SurvivalTime'], dtype=np.int64)
+test_output_public.insert(1,'Event',event_public)
+test_output_public.index.name = 'PatientID'
+
+print(cindex(test_output_real_public, test_output_public))
+
+#on private test:
+test = model.predict(test_base_radiomics_private)
+test_output_private = pd.DataFrame(data = test[:,0], index = id_patient_private, columns=['SurvivalTime'], dtype=np.int64)
+test_output_private.insert(1,'Event',event_private)
+test_output_private.index.name = 'PatientID'
+
+print(cindex(test_output_real_private, test_output_private))
+
+
